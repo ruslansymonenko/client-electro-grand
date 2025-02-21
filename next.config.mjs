9ffc709542727664f186eaa42e622b5478e5042c
ruslansymonenko <symonenkors@gmail.com>
2025-02-21 14:22:13 +0200

    fixed error with revrites routes in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,27 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   env: {
+//     APP_ENV: process.env.APP_ENV,
+//     APP_URL: process.env.APP_URL,
+//     APP_DOMAIN: process.env.APP_DOMAIN,
+//     SERVER_URL: process.env.SERVER_URL,
+//     STAFF_KEY: process.env.STAFF_KEY,
+//   },
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/uploads/:path*',
+//         destination: `${process.env.SERVER_URL}/uploads/:path*`,
+//       }
+//     ]
+//   },
+//   images: {
+//     domains: ['localhost'],
+//   },
+// };
+//
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
@@ -8,12 +32,17 @@ const nextConfig = {
     STAFF_KEY: process.env.STAFF_KEY,
   },
   async rewrites() {
+    if (!process.env.SERVER_URL) {
+      console.warn('⚠️ WARNING: SERVER_URL is not defined! Skipping rewrites.');
+      return [];
+    }
+
     return [
       {
         source: '/uploads/:path*',
         destination: `${process.env.SERVER_URL}/uploads/:path*`,
-      }
-    ]
+      },
+    ];
   },
   images: {
     domains: ['localhost'],
